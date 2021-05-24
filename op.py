76007b0b936e2be53fbc2df677fb266097a0fc4d
NehaPriya21 <81585329+NehaPriya21@users.noreply.github.com>
2021-05-24 10:45:01 +0530

    Update op.py

--- op.py
+++ op.py
@@ -8,7 +8,7 @@ mask = np.zeros(img.shape[:2],np.uint8)
 bgdModel = np.zeros((1,65),np.float64)
 fgdModel = np.zeros((1,65),np.float64)
 
-rect = (50,50,450,290)
+rect = (50,50,450,250)
 cv2.grabCut(img,mask,rect,bgdModel,fgdModel,5,cv2.GC_INIT_WITH_RECT)
 
 mask2 = np.where((mask==2)|(mask==0),0,1).astype('uint8')
